9f69071a7a3f5db4b7f8e7d15bcb1dcf17c85819
H <HARSHITHA.SATHEESKUMAR@GMAIL.COM>
2026-05-22 20:31:56 -0400

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.java
@@ -0,0 +1,42 @@
+class MinStack {
+
+    private ArrayDeque<Integer> stack;
+    private ArrayDeque<Integer> minstack;
+
+
+    public MinStack() {
+        // Initialized a stack object
+        stack = new ArrayDeque<>();
+        minstack = new ArrayDeque<>(); 
+    }
+    
+    public void push(int val) {
+
+        this.stack.push(val);
+
+        if(this.minstack.isEmpty())
+            this.minstack.push(val);
+        else 
+            this.minstack.push(Math.min(val, this.minstack.peek()));
+        
+    }
+    
+    public void pop() {
+
+        this.minstack.pop();
+        this.stack.pop();
+    }
+    
+    public int top() {
+
+        return this.stack.peek();
+
+        
+    }
+    
+    public int getMin() {
+
+        return this.minstack.peek();
+        
+    }
+}
